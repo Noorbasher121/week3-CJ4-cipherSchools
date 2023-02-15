222f9b687fff57119fbbc789e904a20b62bad74d
Noor Basher <122342208+Noorbasher121@users.noreply.github.com>
2023-02-15 22:42:50 +0530

    Add files via upload

---
+++ Stringrotation.java
@@ -0,0 +1,11 @@
+public class Stringrotation {
+    public static boolean areRotataion(String s1,String s2) {
+        if(s1.length()!=s2.length())
+        return false;
+        String str=s1+s2;
+
+        if(str.contains(s2));
+        return true;
+     
+    }
+}
